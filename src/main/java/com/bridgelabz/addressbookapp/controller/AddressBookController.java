476bfe80986dba14c2b0dee2c6f52aee23659ef4
madhu179 <madhubabu232343@gmail.com>
2020-12-14 18:03:58 +0530

    Adidng code to add validations to first,last name, address during rest call

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -2,6 +2,8 @@ package com.bridgelabz.addressbookapp.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +45,7 @@ public class AddressBookController {
 	}
 	
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO> createAddressBookData(@RequestBody AddressBookDTO addressBookDTO){
+	public ResponseEntity<ResponseDTO> createAddressBookData(@Valid @RequestBody AddressBookDTO addressBookDTO){
 		AddressBookData contactData = null;
 		contactData = addressBookService.createAddressBookData(addressBookDTO);
 		ResponseDTO respDTO = new ResponseDTO("Created AddressBook Data Successfully", contactData);
@@ -51,7 +53,8 @@ public class AddressBookController {
 	}
 	
 	@PutMapping("/update/{id}")
-	public ResponseEntity<ResponseDTO> updateAddressBookData(@PathVariable("id") int id, @RequestBody AddressBookDTO addressBookDTO){
+	public ResponseEntity<ResponseDTO> updateAddressBookData(@PathVariable("id") int id,
+																@Valid	@RequestBody AddressBookDTO addressBookDTO){
 		AddressBookData contactData = null;
 		contactData = addressBookService.updateAddressBookData(id,addressBookDTO);
 		ResponseDTO respDTO = new ResponseDTO("Updated AddressBook Data Successfully", contactData);
--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -1,9 +1,16 @@
 package com.bridgelabz.addressbookapp.dto;
 
+import javax.validation.constraints.Pattern;
+
 public class AddressBookDTO {
 	
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message="First Name Invalid")
 	public String firstName;
+	
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message="Last Name Invalid")
 	public String lastName;
+	
+	@Pattern(regexp = "[A-Za-z,/:.0-9\\s]{3,}", message="Address Invalid")
 	public String address;
 
 	public AddressBookDTO(String firstName, String lastName, String address) {
