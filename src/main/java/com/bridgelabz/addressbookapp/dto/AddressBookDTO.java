cdfdf932fb2fc5008b570bee6ff0e2689362de50
madhu179 <madhubabu232343@gmail.com>
2020-12-16 19:18:52 +0530

    Adding code to add validations in AddressBook DTO

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbookapp.dto;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import lombok.ToString;
@@ -15,10 +16,19 @@ public @ToString class AddressBookDTO {
 	@Pattern(regexp = "[A-Za-z,/:.0-9\\s]{3,}", message="Address Invalid")
 	public String address;
 	
+	@NotNull(message="city should not be Empty")
 	public String city;
+	
+	@NotNull(message="state should not be Empty")
 	public String state;
+	
+	@Pattern(regexp = "^[0-9]{3}[\\s]*[0-9]{2,}$", message="Zip Invalid")
 	public String zip;
+	
+	@Pattern(regexp = "^[1-9][0-9]{9}$", message="Phone Number Invalid")
 	public String phone;
+	
+	@Pattern(regexp = "^[a-zA-Z]+[a-zA-Z_+.-]*[a-zA-Z]+@[a-zA-Z]+[.][a-zA-z]{2,}$", message="Email Invalid")
 	public String email;
 
 }
